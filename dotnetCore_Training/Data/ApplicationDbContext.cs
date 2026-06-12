b6a80ee5c79fdde2d99c84f68d462b0a967433b4
ashishkhadye33 <ashishkhadye33@gmail.com>
2026-06-12 12:27:13 +0530

    dotnetCore session 3

---
+++ dotnetCore_Training/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using dotnetCore_Training.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetCore_Training.Data
+{
+    public class ApplicationDbContext : DbContext //efcore
+    {
+        //constructor is called when object is created/initialized
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        //DB9
+        public DbSet<Student> Students { get; set; }
+
+
+    }
+}
--- dotnetCore_Training/Models/Student.cs
+++ dotnetCore_Training/Models/Student.cs
@@ -8,5 +8,7 @@
         public string Name { get; set; }
 
         public string City { get; set; }
+
+
     }
 }
