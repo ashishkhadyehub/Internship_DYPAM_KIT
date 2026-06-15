9a0c6204212aea62395f557dba4d5e73a74c20be
ashishkhadye33 <ashishkhadye33@gmail.com>
2026-06-15 12:37:46 +0530

    dotnet_core session 4

---
+++ dotnetCore_Training/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using dotnetCore_Training.Data;
+using dotnetCore_Training.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetCore_Training.Controllers
+{
+    public class StudentController : Controller
+    {
+        //select field and press ctrl + .
+        private readonly ApplicationDbContext _context;
+
+        //constructor dependency injection
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //template - UI design
+        //model - model class
+        public IActionResult Index()
+        {
+                            //database.tablename.selctquery
+            var students = _context.Students.ToList();
+            return View(students);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- dotnetCore_Training/Models/Student.cs
+++ dotnetCore_Training/Models/Student.cs
@@ -5,7 +5,7 @@
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string Name { get; set; } = "Dafault Name";
 
         public string City { get; set; }
 
