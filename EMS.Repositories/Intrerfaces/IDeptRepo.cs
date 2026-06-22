b63008f431f185e2c7b936719481e5280c4b61c5
ashishkhadye33 <ashishkhadye33@gmail.com>
2026-06-22 13:09:49 +0530

    dotnetCore_project_day1

---
+++ EMS.Repositories/Intrerfaces/IDeptRepo.cs
@@ -0,0 +1,22 @@
+﻿using EMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Repositories.Intrerfaces
+{
+    public interface IDeptRepo
+    {
+        IEnumerable<Department> GetAll();
+
+        Department GetById(int id);
+
+        void Save(Department department);
+
+        void Edit(Department department);
+
+        void RemoveData(Department department);
+    }
+}
---
+++ EMS.UI/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.UI.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
